c929333d93fb52fec78a142e87c7d3c2d38d340c
bope0915 <bogdan.innova@gmail.com>
2015-11-11 15:23:45 +0200

    Initial commit

---
+++ src/Calculator.java
@@ -0,0 +1,14 @@
+i
+public class Calculator {
+
+    public static final double OMEGA = 0.5;
+    public static final double FIELDS_MODULE = 1;
+
+    public double getDerPsi(double t) {
+        if (t < (Math.PI / (2 * OMEGA)))
+            return FIELDS_MODULE;
+        while (true)
+
+    }
+
+}
