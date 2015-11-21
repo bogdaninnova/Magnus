59be36880dd77dd98b96824c026da72541b4421e
bogdaninnova <bogdan.innova@gmail.com>
2015-11-21 13:35:09 +0200

    update

--- src/Calculator.java
+++ src/Calculator.java
@@ -6,37 +6,33 @@ public class Calculator {
 	private static final double ETA = 0.001;
 	private static final double RO = 1;
 	private static final double A = Math.pow(10, -5);
+	private static double ALPHA = 1;//T * M * H / 6 / ETA;
+	private static double BETTA = 1;//RO * M * H * A * A / 36 / ETA / ETA;	
 	
-	private static double ALPHA = T * M * H / 6 / ETA;
-	private static double BETTA = RO * M * H * A * A / 36 / ETA / ETA;	
-	
-	private static double PSI_0 = - ALPHA / 4;
-		
-	private final double dt = 0.0001;
-
-	private double t = 0;
+	private static double PSI_0 = 1;
 	public double ksi = 0;
-
+	private final double dt = 0.001;
+	private double t = 0;
+	
 	public double ux = 0;
 	public double uy = 0;
-
-
-	public void iteration() {
+	
+	public void EulerIteration() {
+		ksi += dt * equation(t, ksi);
+		t += dt;
+	}
+	
+	public void RungeKuttIteration() {
 		double k1 = equation(t, ksi);
 		double k2 = equation(t + dt / 2, ksi + k1 * dt / 2);
 		double k3 = equation(t + dt / 2, ksi + k2 * dt / 2);
 		double k4 = equation(t + dt, ksi + k3 * dt);
 		ksi += dt / 6 * (k1 + 2 * k2 + 2 * k3 + k4);
 		t += dt;
-
-		ux += getUx();
-		uy += getUy();
 	}
 
-
-	
 	private static double equation(double t, double ksi) {
-		return - ALPHA * Math.sin(ksi * Math.PI / 180) - getDerivatePsi(t);
+		return - ALPHA * Math.sin(ksi) - getDerivatePsi(t);
 	}
 	
 	private static double getDerivatePsi(double t) {
@@ -46,7 +42,7 @@ public class Calculator {
 			if ((t >= n - 1) && (t < n - 0.5))
 				return 4 * PSI_0;
 			if ((t > n - 0.5) && (t < n))
-				return -4 * PSI_0;
+				return - 4 * PSI_0;
 		}
 	}
 	
@@ -58,4 +54,5 @@ public class Calculator {
 	public double getUy() {
 		return - BETTA * Math.sin(ksi) * getUx();
 	}
+
 }
--- src/Main.java
+++ src/Main.java
@@ -4,67 +4,41 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-        
-    	
-    	
+
     	Calculator calc = new Calculator();
-
-    	ArrayList<Double> listX = new ArrayList<Double>(10000);
-    	ArrayList<Double> listY = new ArrayList<Double>(10000);
 		ArrayList<Double> listKsi = new ArrayList<Double>(10000);
-
-    	for (int i = 0; i < 100000; i++)
-    		calc.iteration();
-
-
-
-    	for (int i = 0; i < 30000; i++) {
-    		calc.iteration();
-			listX.add(calc.getUx());
-			listY.add(calc.getUy());
+		ArrayList<Double> listX = new ArrayList<Double>(10000);
+		ArrayList<Double> listY = new ArrayList<Double>(10000);
+		
+		
+		for (int i = 0; i < 100000; i++)
+			calc.RungeKuttIteration();
+		
+    	for (int i = 0; i < 5000; i++) {
+    		calc.RungeKuttIteration();
+    		calc.ux += calc.getUx();
+    		calc.uy += calc.getUy();
 			listKsi.add(calc.ksi);
+			listX.add(calc.ux);
+			listY.add(calc.uy);
 	   	}
 
-
 		writeDoubleList(listKsi, "listKsi");
-    	writeDoubleList(listX, "listX");
-    	writeDoubleList(listY, "listY");
+		writeDoubleList(listX, "listX");
+		writeDoubleList(listY, "listY");
     }
-	/*
-   	for (int i = 0; i < 100000; i++) {
-	if (counter++ == 1000) {
-		counter = 0;
-		listX.add(calc.getAverrageUx());
-		listY.add(calc.getAverrageUy());
-		calc.ux = 0;
-		calc.uy = 0;
-		calc.counter = 0;
-	}
-	calc.iteration();
-
-}
- * */
     
-	@SuppressWarnings("resource")//damn
+	@SuppressWarnings("resource")
 	public static void writeDoubleList(List<Double> averrageList, String name) {
-		
 		ListIterator<Double> iter = averrageList.listIterator();
-		
 		File file = new File(name + ".txt");
 		try {
 			FileWriter writer = new FileWriter(file);
-
-			while (iter.hasNext()) writer.append(iter.next() + "\r\n");
-			
+			while (iter.hasNext())
+				writer.append(iter.next() + "\r\n");
 			writer.flush();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-
-
-
-
-
-
 }
