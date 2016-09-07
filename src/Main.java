f8486d9d211df4be12c2e8c2d8929bcebe6cbf48
bogdaninnova <bogdan.innova@gmail.com>
2016-09-07 11:40:44 +0300

    Added history writing

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,35 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 public class Main {
 
+
+
+
+
+
+
+    private static void saveCodeInHistory() {
+        String dir = System.getProperty("user.dir");
+        ZipUtils appZip = new ZipUtils();
+        appZip.generateFileList(new File(dir + "\\src"), dir + "\\src");
+        appZip.zipIt("history\\" + new Date().getTime() + ".zip", dir + "\\src");
+    }
+
+
+
+
+
     public static void main(String[] args) {
+        saveCodeInHistory();
 
 //        double p = 0.001;
 //        for (double psi = 1; psi <= 1; psi += 10) {
@@ -43,9 +67,9 @@ public class Main {
 //        System.exit(0);
 
         double phase = 0;
-        for (double alpha = 0.01; alpha <= 0.01; alpha = round(alpha + 0.1, 3)) {
+        for (double alpha = 0.001; alpha < 0.1; alpha = round(alpha + 0.001, 3)) {
             ArrayList<Double> listS = new ArrayList<>();
-            for (double psi = 0; psi <= 500; psi += 10) {
+            for (double psi = 0; psi <= 100; psi += 1) {
                 System.out.println(new Date());
                 System.out.println("alpha = " + alpha);
                 System.out.println("psi = " + psi);
@@ -56,7 +80,6 @@ public class Main {
                 while (calc.t < time+0.5)
                     calc.iteration(true);
                 listS.add(calc.sGamma);
-                System.out.println(psi);
             }
             //ew.addColumn(name, listS);
             TextWriter.writeDoubleList(listS, "alpha = " + alpha);
